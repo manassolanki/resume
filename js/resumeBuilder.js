49ddafff8b86a3f49494b6d784142b5e19e37ebb
Manas Solanki <manas@erpnext.com>
2017-01-21 21:49:09 +0530

    First commit

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,15 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+// $("#main").append("Manas Solanki \n");
+var name = "Manas Solanki";
+console.log(name);
+var awesomeThoughts = "I am Manas Solanki and I am awesome!";
+console.log(awesomeThoughts);
+
+$( "#main" ).append(awesomeThoughts);
+
+var awesome = awesomeThoughts.replace("awesome", "fun");
+console.log(awesome);
+
+$( "#main" ).append("<br>"+awesome);
