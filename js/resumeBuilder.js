c6f842d5f442d1fcd4db41f4a66533ae68b6165d
Manas Solanki <manas@erpnext.com>
2017-01-22 23:28:40 +0530

    Adding Objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,15 +1,31 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
-// $("#main").append("Manas Solanki \n");
+
+// console.log(name);
+// var awesomeThoughts = "I am Manas Solanki and I am awesome!";
+// console.log(awesomeThoughts);
+
+// $( "#main" ).append(awesomeThoughts);
+// var awesome = awesomeThoughts.replace("awesome", "fun");
+// console.log(awesome);
+// $( "#main" ).append("<br>"+awesome);
+
 var name = "Manas Solanki";
-console.log(name);
-var awesomeThoughts = "I am Manas Solanki and I am awesome!";
-console.log(awesomeThoughts);
+var role = "Software Developer";
+var skills = ["Programming", "HTML", "CSS", "Javascript", "Python", "C++"];
+var bio = {
+	"name": "Manas Solanki",
+	"role": "Software Developer",
+	"contact": "manassolanki@gmail.com",
+	"pic": "images/dp.jpg",
+	"welcome": "Welcome Message",
+	"skills": skills
+};
 
-$( "#main" ).append(awesomeThoughts);
+var formattedname = HTMLheaderName.replace("%data%", name);
+var formattedrole = HTMLheaderRole.replace("%data%", role);
 
-var awesome = awesomeThoughts.replace("awesome", "fun");
-console.log(awesome);
+$("#header").prepend(formattedrole);
+$("#header").prepend(formattedname);
 
-$( "#main" ).append("<br>"+awesome);
